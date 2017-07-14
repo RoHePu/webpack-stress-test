d21b0d4e190de23744b1534b07dfd6a7ea10da2f
Herrmann <robin.herrmann@sap.com>
2017-07-14 10:56:30 +0200

    added sourcemaps config

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const webpack = require("webpack");
 
 module.exports = {
     entry: "./src",
+    devtool: "cheap-eval-source-map",
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].chunk.js"
